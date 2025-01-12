a6f03a4219541292222f893ff6e22f226d8d7029
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2025-01-12 20:23:48 +0530

    Set up Express server for production and configure Vite base path for GitHub Pages

--- frontend/todoApp/vite.config.ts
+++ frontend/todoApp/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/toDoApp/',  // Set the base to the GitHub Pages subdirectory
   plugins: [react()],
 })
