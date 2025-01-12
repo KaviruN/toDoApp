ba1044e954c85c0388c1987125588eca9e089666
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2025-01-12 20:45:35 +0530

    Update Vite configuration and add homepage for GitHub Pages deployment

--- frontend/todoApp/vite.config.ts
+++ frontend/todoApp/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/toDoApp/',  // Set the base to the GitHub Pages subdirectory
+export default defineConfig({  // Set the base to the GitHub Pages subdirectory
   plugins: [react()],
+  base: '/toDoApp',
 })
