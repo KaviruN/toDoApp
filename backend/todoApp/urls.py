c7a715263b9ea50b500ed369c13c34a164741430
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2025-06-15 01:34:47 +0530

    Refactor URL routing: update todo endpoint and remove 'api/' prefix from paths

--- backend/todoApp/urls.py
+++ backend/todoApp/urls.py
@@ -6,7 +6,7 @@ from todo import views
 from auths.views import UserCreate, MyTokenObtainPairView
 
 router = routers.DefaultRouter()
-router.register(r'api/todo', views.TodoViewSet, basename='todo')
+router.register(r'todo', views.TodoViewSet, basename='todo')
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -16,8 +16,8 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/register/', UserCreate.as_view()),
+    path('/', include('rest_framework.urls', namespace='rest_framework')),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', UserCreate.as_view()),
 ]
